ad07aa4102c465058ada32560c3c756c23f26a66
DESKTOP-M48INUK\injurys <1762910894@qq.com>
2020-12-23 17:52:42 +0800

    合并方法

--- functions/tools.php
+++ functions/tools.php
@@ -1,6 +1,113 @@
 <?php
 
 /**
+ * @Description:  抛出异常
+ * @Author: yashuai
+ * @param string $code 状态码
+ * @param string $msg 提示信息
+ */
+if (!function_exists('_throw')) {
+    function _throw($code = 500, $msg = '')
+    {
+        throw new \Exception($msg, $code);
+    }
+}
+
+/**
+ * @Description:  成功返回
+ * @Author: yashuai
+ * @param string $data 返回数据
+ * @param string $msg 提示信息
+ */
+if (!function_exists('_success')) {
+    function _success($data = [], $msg = 'success')
+    {
+        echo json_encode(['code' => 0, 'msg' => $msg, 'data' => $data]);
+        exit;
+    }
+}
+
+/**
+ * @Description: 失败返回
+ * @Author: yashuai
+ * @param string $msg 提示信息
+ * @param int $code 状态码
+ * @param string $data 返回数据
+ */
+if (!function_exists('_fail')) {
+    function _fail($code = 500, $msg = 'fail', $data = [])
+    {
+        echo json_encode(['code' => $code, 'msg' => $msg, 'data' => $data]);
+        exit;
+    }
+}
+
+/**
+ * @Desc 计算两个日期相差时间
+ * @param string $startTime
+ * @param string $endTime
+ * @param int $type 0:天数  1:小时数  2:分钟数 3:秒数 4:秒数
+ * @author yashuai
+ * @Time 2020/12/7 14:33
+ */
+if (!function_exists('_diff_between_days')) {
+    function _diff_between_days($start, $end, $type = 0)
+    {
+        $interval = [86400, 3600, 60, 0];
+        $type     = $interval[$type] ?? $interval[0];
+        $res      = strtotime($end) - strtotime($start);
+        return $type ? (int)$res / $type : $res;
+    }
+}
+
+/**
+ * @Desc  把返回的数据集转换成Tree
+ * @author yashuai
+ * @param array $list 原始数据
+ * @param string $pk 主键
+ * @param string $pid 父级ID
+ * @param string $child 子级索引
+ * @param int $root
+ * @return array
+ */
+if (!function_exists('_listToTree')) {
+    function _listToTree($list = [], $pk = 'id', $pid = 'pid', $child = '_child')
+    {
+        $tree = array();  //格式化好的树
+        foreach ($list as $item) {
+            if (isset($list[$item[$pid]])) {
+                $list[$item[$pid]][$child][] = &$list[$item[$pk]];
+            } else {
+                $tree[] = &$list[$item[$pk]];
+            }
+        }
+        return $tree;
+    }
+}
+
+/**
+ * @Desc  获取数组字段值
+ * @author yashuai
+ * @param array $array 数组
+ * @param string $field 字段名称
+ * @param bool|string|integer $default 默认值
+ * @param bool|string|integer|array $exception 例外值
+ * @return bool|mixed
+ */
+if (!function_exists('_array_field')) {
+    function _array_field($array = [], $field = '', $default = false, $exception=true)
+    {
+        if(!isset($array[$field])){
+            return $default;
+        }
+        $value = $array[$field];
+        return empty($value)
+            ? (empty($exception) ? ($value === $exception ? $value : $default) : $default)
+            : $value;
+    }
+}
+
+/**
  * CMF密码加密方法
  * @param string $password 要加密的字符串
  * @param string $key 加密秘钥
@@ -61,4 +168,5 @@ if (!function_exists('_randString')) {
         $chars = str_shuffle($chars);
         return substr($chars, 0, $len);
     }
+
 }
